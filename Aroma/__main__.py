50806cd48b06b126b1bd056fc4048387ca60686a
Ronin <bhabyaprakash01@gmail.com>
2024-10-29 09:48:07 +0530

    Update __main__.py

--- Aroma/__main__.py
+++ Aroma/__main__.py
@@ -5,15 +5,16 @@ from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from Aroma import LOGGER, app, userbot
-from Aroma.core.call import Anony
-from Aroma.misc import sudo
-from Aroma.plugins import ALL_MODULES
-from Aroma.utils.database import get_banned_users, get_gbanned
+from ComboBot import LOGGER, app, userbot
+from ComboBot.core.call import Mukesh
+from ComboBot.misc import sudo
+from ComboBot.plugins import ALL_MODULES
+from ComboBot.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
-
+from ComboBot.plugins.bot.clone import restart_bots
 
 async def init():
+
     if (
         not config.STRING1
         and not config.STRING2
@@ -33,29 +34,28 @@ async def init():
             BANNED_USERS.add(user_id)
     except:
         pass
+    await restart_bots()
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("Aroma.plugins" + all_module)
-    LOGGER("Aroma.plugins").info("Successfully Imported Modules...")
+        importlib.import_module("ComboBot.plugins" + all_module)
+    LOGGER("ComboBot.plugins").info("Successfully Imported Modules...")
     await userbot.start()
-    await Anony.start()
+    await Mukesh.start()
     try:
-        await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+        await Mukesh.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
-        LOGGER("Aroma").error(
+        LOGGER("ComboBot").error(
             "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
         )
-        exit()
+        # exit()
     except:
         pass
-    await Anony.decorators()
-    LOGGER("Aroma").info(
-        "\x41\x6e\x6f\x6e\x58\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\n\n\x44\x6f\x6e'\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x46\x61\x6c\x6c\x65\x6e\x41\x73\x73\x6f\x63\x69\x61\x74\x69\x6f\x6e"
-    )
+    await Mukesh.decorators()
+
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("Aroma").info("Stopping Aroma Bot...")
+    LOGGER("ComboBot").info("Stopping Combo Bot...")
 
 
 if __name__ == "__main__":
