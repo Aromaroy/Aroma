240c99c500d317e321063178e6a6b82aa686f0b9
Ronin <bhabyaprakash01@gmail.com>
2024-11-01 22:25:45 +0530

    Update mute.py

--- Aroma/plugins/management/mute.py
+++ Aroma/plugins/management/mute.py
@@ -66,7 +66,8 @@ async def mute_user(client, message):
         permissions = ChatPermissions(
             can_send_messages=False,
             can_send_media_messages=False,
-            can_use_inline_bots=False,
+            can_send_polls=False,
+            can_send_other_messages=False,
             can_add_web_page_previews=False,
             can_invite_to_chats=False,
             can_pin_messages=False
