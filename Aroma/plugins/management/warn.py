9d756c1b2d193ffd2a3746ef853153004623dd7c
Ronin <bhabyaprakash01@gmail.com>
2024-11-02 22:57:40 +0530

    Update warn.py

--- Aroma/plugins/management/warn.py
+++ Aroma/plugins/management/warn.py
@@ -126,4 +126,4 @@ async def remove_warning(client, query):
     else:
         await query.answer("No warnings to remove for this user.", show_alert=False)
 
-    await client.send_message(chat_id, f"Admin {query.from_user.mention} has removed {target_user_id}'s warning.")
+    await client.send_message(chat_id, f"Admin {query.from_user.mention} has removed {target_user_id}'s warning. Remaining warnings: {user_record['warnings'] - 1}/3.")
