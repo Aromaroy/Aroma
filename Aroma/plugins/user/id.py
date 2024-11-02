e92c74e23bc9297c1299b4d4aece35904d84887e
Ronin <bhabyaprakash01@gmail.com>
2024-11-02 18:09:42 +0530

    Update id.py

--- Aroma/plugins/user/id.py
+++ Aroma/plugins/user/id.py
@@ -9,33 +9,41 @@ async def get_id(client: Client, message):
     message_id = message.id
     reply = message.reply_to_message
 
-    text = f"**[Message ID:]({message.link})** `{message_id}`\n"
-    text += f"**[Your ID:](tg://user?id={your_id})** `{your_id}`\n"
+    try:
+        processing_message = await message.reply("Fetching IDs...")
 
-    if len(message.command) == 2:
-        try:
-            split = message.text.split(None, 1)[1].strip()
-            user_id = (await client.get_users(split)).id
-            text += f"**[User ID:](tg://user?id={user_id})** `{user_id}`\n"
-        except Exception:
-            text = "This user doesn't exist."
-            await message.reply(text)
-            return
+        text = f"**[Message ID:]({message.link})** `{message_id}`\n"
+        text += f"**[Your ID:](tg://user?id={your_id})** `{your_id}`\n"
 
-    text += f"**[Chat ID:](https://t.me/{chat.username})** `{chat.id}`\n\n"
+        if len(message.command) == 2:
+            try:
+                split = message.text.split(None, 1)[1].strip()
+                user_id = (await client.get_users(split)).id
+                text += f"**[User ID:](tg://user?id={user_id})** `{user_id}`\n"
+            except Exception:
+                text = "This user doesn't exist."
+                await processing_message.edit(text)
+                return
 
-    if reply and not reply.forward_from_chat and not reply.sender_chat:
-        text += f"**[Replied Message ID:]({reply.link})** `{reply.id}`\n"
-        text += f"**[Replied User ID:](tg://user?id={reply.from_user.id})** `{reply.from_user.id}`\n\n"
+        text += f"**[Chat ID:](https://t.me/{chat.username})** `{chat.id}`\n\n"
 
-    if reply and reply.forward_from_chat:
-        text += f"The forwarded channel, {reply.forward_from_chat.title}, has an ID of `{reply.forward_from_chat.id}`\n\n"
+        if reply and not reply.forward_from_chat and not reply.sender_chat:
+            text += f"**[Replied Message ID:]({reply.link})** `{reply.id}`\n"
+            text += f"**[Replied User ID:](tg://user?id={reply.from_user.id})** `{reply.from_user.id}`\n\n"
 
-    if reply and reply.sender_chat:
-        text += f"ID of the replied chat/channel is `{reply.sender_chat.id}`"
+        if reply and reply.forward_from_chat:
+            text += f"The forwarded channel, {reply.forward_from_chat.title}, has an ID of `{reply.forward_from_chat.id}`\n\n"
 
-    await message.reply(
-        text,
-        disable_web_page_preview=True,
-        parse_mode=ParseMode.DEFAULT,
-    )
+        if reply and reply.sender_chat:
+            text += f"ID of the replied chat/channel is `{reply.sender_chat.id}`"
+
+        await message.reply(
+            text,
+            disable_web_page_preview=True,
+            parse_mode=ParseMode.DEFAULT,
+        )
+
+    except Exception as e:
+        await message.reply(f"Error: {e}")
+
+Remove the code which say fetching it it will directly provide the data
