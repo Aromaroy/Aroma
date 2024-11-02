446fa240197f2b89e408112ed084c21388bcd7e3
Ronin <bhabyaprakash01@gmail.com>
2024-11-02 17:59:27 +0530

    Update id.py

--- Aroma/plugins/user/id.py
+++ Aroma/plugins/user/id.py
@@ -1,46 +1,55 @@
-import logging
-from pyrogram import Client, filters
+import asyncio
+from pyrogram import filters, Client
+from pyrogram.enums import ParseMode
 from Aroma import app
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-async def get_target_user_id(client, message):
-    if message.reply_to_message:
-        return message.reply_to_message.from_user.id
-    elif message.command[1:]:
-        target_username = message.command[1]
-        user = await client.get_users(target_username)
-        return user.id
-    else:
-        return None
+async def delete_message_after_delay(message, delay):
+    await asyncio.sleep(delay)
+    await message.delete()
 
 @app.on_message(filters.command('id'))
-async def id_user(client, message):
-    chat_id = message.chat.id if message.chat else message.from_user.id
-    logger.info(f"Chat ID: {chat_id}")
-
-    target_user_id = await get_target_user_id(client, message)
-    if target_user_id is None:
-        await client.send_message(chat_id, "Could not find the target user.")
-        return
+async def get_id(client: Client, message):
+    chat = message.chat
+    your_id = message.from_user.id
+    message_id = message.id
+    reply = message.reply_to_message
 
     try:
-        target_user = await client.get_users(target_user_id)
-        target_name = target_user.first_name + (f" {target_user.last_name}" if target_user.last_name else "")
-        user_id_message = f"The ID of `{target_name}` is `{target_user_id}`."
+        processing_message = await message.reply("Fetching IDs...")
 
-        # Check if in a group or channel and fetch their ID
-        if message.chat:
-            chat_type = message.chat.type
-            if chat_type in ['group', 'supergroup']:
-                group_id = message.chat.id
-                user_id_message += f"\nThe Group ID is `{group_id}`."
-            elif chat_type == 'channel':
-                channel_id = message.chat.id
-                user_id_message += f"\nThe Channel ID is `{channel_id}`."
+        text = f"**[Message ID:]({message.link})** `{message_id}`\n"
+        text += f"**[Your ID:](tg://user?id={your_id})** `{your_id}`\n"
 
-        await client.send_message(chat_id, user_id_message)
+        if len(message.command) == 2:
+            try:
+                split = message.text.split(None, 1)[1].strip()
+                user_id = (await client.get_users(split)).id
+                text += f"**[User ID:](tg://user?id={user_id})** `{user_id}`\n"
+            except Exception:
+                text = "This user doesn't exist."
+                await processing_message.edit(text)
+                await delete_message_after_delay(processing_message, 20)
+                return
+
+        text += f"**[Chat ID:](https://t.me/{chat.username})** `{chat.id}`\n\n"
+
+        if reply and not reply.forward_from_chat and not reply.sender_chat:
+            text += f"**[Replied Message ID:]({reply.link})** `{reply.id}`\n"
+            text += f"**[Replied User ID:](tg://user?id={reply.from_user.id})** `{reply.from_user.id}`\n\n"
+
+        if reply and reply.forward_from_chat:
+            text += f"The forwarded channel, {reply.forward_from_chat.title}, has an ID of `{reply.forward_from_chat.id}`\n\n"
+
+        if reply and reply.sender_chat:
+            text += f"ID of the replied chat/channel is `{reply.sender_chat.id}`"
+
+        await message.reply(
+            text,
+            disable_web_page_preview=True,
+            parse_mode=ParseMode.DEFAULT,
+        )
+
     except Exception as e:
-        await client.send_message(chat_id, f"Failed to retrieve user ID: {str(e)}")
-        logger.error(f"Failed to retrieve user ID: {str(e)}")
+        await message.reply(f"Error: {e}")
+
+    await delete_message_after_delay(processing_message, 20)
