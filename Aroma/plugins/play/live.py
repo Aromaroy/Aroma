11545ab3d956a0ea5a428be8ad7a863de2a8a371
Ronin <bhabyaprakash01@gmail.com>
2024-11-04 16:28:51 +0530

    Update live.py

--- Aroma/plugins/play/live.py
+++ Aroma/plugins/play/live.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
 
-from Aroma import YouTube, app
+from aroma import YouTube, app
 from Aroma.utils.channelplay import get_channeplayCB
 from Aroma.utils.decorators.language import languageCB
 from Aroma.utils.stream.stream import stream
